893f82ec35fe39041f6997cbc67a24730bf05e40
Chaitra Dhake <chaitradhake21@gmail.com>
2026-06-14 00:32:03 +0530

    remove config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// Environment variables are now handled securely by the Vercel serverless backend at /api/contact.js.
-// This file is kept with empty placeholders to prevent index.html references from throwing errors.
-const BOT_TOKEN = "";
-const CHAT_ID = "";
